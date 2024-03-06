e6e3012485153550c2908e4e4aa39019461e6bf3
Naelerd <juampi_22_55@hotmail.com>
2024-03-06 15:08:33 -0300

    Commit 1.0

---
+++ src/main/java/com/app/sistemavacacional/SistemaVacacionalAPP.java
@@ -0,0 +1,12 @@
+package com.app.sistemavacacional;
+
+public class SistemaVacacionalAPP {
+
+    public static void main(String[] args) {
+        Bienvenida ventanabienvenida = new Bienvenida();
+        ventanabienvenida.setBounds(0, 0, 350, 450);
+        ventanabienvenida.setVisible(true);
+        ventanabienvenida.setResizable(false);
+        ventanabienvenida.setLocationRelativeTo(null);
+    }
+}
